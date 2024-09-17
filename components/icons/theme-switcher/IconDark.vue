902a3fa852738775ccbb9b747fad09d06dff7498
Jacem Chaieb <jacem@chaieb.me>
2024-09-17 13:59:34 +0200

    feat: update dark mode icon

--- components/icons/theme-switcher/IconDark.vue
+++ components/icons/theme-switcher/IconDark.vue
@@ -1,16 +1,19 @@
 <template>
-  <!-- https://icon-sets.iconify.design/hugeicons/moon-01/ -->
+  <!-- https://icon-sets.iconify.design/hugeicons/moon-02/ -->
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-  ><g
-    fill="none"
-    stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    color="currentColor"
-  ><path d="M21.099 7.845a3.5 3.5 0 1 1-4.943-4.943m4.943 4.943A10 10 0 0 1 22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2c1.483 0 2.89.323 4.155.901M21.1 7.845A10.04 10.04 0 0 0 16.155 2.9M10 8h.006M7 14h.006" /><path d="M16 16a2 2 0 1 1-4 0a2 2 0 0 1 4 0" /></g></svg>
+  >
+    <path
+      fill="none"
+      color="currentColor"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M21.5 14.078A8.557 8.557 0 0 1 9.922 2.5C5.668 3.497 2.5 7.315 2.5 11.873a9.627 9.627 0 0 0 9.627 9.627c4.558 0 8.376-3.168 9.373-7.422"
+    />
+  </svg>
 </template>
